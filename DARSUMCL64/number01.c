fa8f0649bab6511dd7bfb8208a89992485aae64a
Davi Rocha <davirocha.x.2.0@gmail.com>
2026-03-13 21:27:24 +0000

    somemore

--- DARSUMCL64/number01.c
+++ DARSUMCL64/number01.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
 int main(void){
-    printf("hello world");
-    return 0;
-}
+    int a, b;
+
+    do {
+        printf("a:");
+        scanf("%d", &a);
+        
+        printf("b:");
+        scanf("%d", &b);
+    } while (b == 0);
+    
+    printf("a/b = %d\n", a/b);
+    
+    return 0; 
+
+    }
