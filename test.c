606f2ce9fb1769a21aa0ffa63bfe2fee56658bb9
Davi Rocha <davirocha.x.2.0@gmail.com>
2026-03-13 14:21:19 +0000

    pure aura

---
+++ test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+int main (){
+    double a, b, c, delta;
+
+    printf("a: "); scanf("%lf", &a);
+    printf("b: "); scanf("%lf", &b);
+    printf("c: "); scnaf("%lf", &c);
+
+    if (delta < 0)
