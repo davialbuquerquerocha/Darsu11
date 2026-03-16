db140bf485f5b4942436d673c9daa07e75a46b71
Davi Rocha <davirocha.x.2.0@gmail.com>
2026-03-16 11:17:06 +0000

    filesaved

--- DARSUMCL64/number02.c
+++ DARSUMCL64/number02.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
+#include <math.h>
 
 int main (void)
 {
     int a, b;
+
+        printf("a:");
+        scanf("%d", &a);
+    do {
+        printf("b:");
+        scanf("%d", &b);
+    } while (b < 0);
+    printf("aˆb = %d\n"), (int)pow(a, b);
+    return 0;
+    }
     
-    do
+    
     
     
