1fd563234f0d9a9f7ad488fbc16473dcd4e57e0c
Davi Rocha <davirocha.x.2.0@gmail.com>
2026-04-21 17:48:12 +0000

    last update

--- DARSUMCL64/number01.c
+++ DARSUMCL64/number01.c
@@ -1,25 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void){
-    double a, b;
+void sum(int *a, int *b, int *c){
+    *c = *a + *b;
+}
 
-    do {
-        printf("a:");
-        scanf("%lf", &a);
-        
-        printf("b:");
-        scanf("%lf", &b);
-    } while (b == 0);
+void mines(int* a, int* b, int* c){
+    *c = *a + *b;
+}
+
+int main(int argc, char* argv[]){
     
-    char y[64] = a/b;
-    for (int i = 64; i >= 0; i--){
-        if (y[i] == 44){
-            printf y[i];
-        } else {
-            if ()
-        }
+    if (argc < 2){
+        return 1;
     }
-    
-    return 0; 
+    int as = atoi(&argv[1][0]);
+    int bs = atoi(&argv[1][2]);
+    int cs = 0;
 
+    int *a = &as;
+    int *b = &bs;
+    int *c = &cs;
+
+    if (argv[1][1] == '-'){
+        mines(a, b, c);
+    } else if (argv[1][1] == '+'){
+        sum(a, b, c);
+    }
+    printf("\n%d\n\n", cs);
+
+return 0;
 }
